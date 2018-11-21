b4bfd3fb54b4a38d2da5869f6cffcd85a9863a25
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-11-20 23:24:58 -0500

    User model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {type: String, required: true},
+  description: String,
+  duration: Number,
+  date: Date
+  // _id is automatically included
+}, {timestamps: true});
+
+const ModelClass = mongoose.model('User', userSchema)
+
+module.exports = ModelClass; 
+
+// TEMPLATE:
+// {
+//   username: "Alejahd",
+//   description: "Eat Fatty Foods",
+//   duration: 60,
+//   _id: "BJ67nLMAm",
+//   date: "Tue Mar 19 2019"
+//   }
