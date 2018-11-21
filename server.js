f104650fc96842ef8ee06235e2733822db4ff462
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-11-21 14:50:55 -0500

    code for limit to from queries written

--- server.js
+++ server.js
@@ -16,14 +16,37 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
-// TESTER GET
+// INTIAL TESTER GET
 server.get("/", (req, res) => {
   res.json({ api: "exercise tracker server up and running" });
 });
 
-// View All Users
-server.get("/api/users/", (req, res) => {
-  User.find()
+// Log All Users
+// Ex. http://localhost:5000/api/exercise/log?id=5bf4e9f848cbb2a630259995&limit=1&from=2019-09-09&duration=10&to=2019-09-09
+server.get("/api/exercise/log", (req, res) => {
+  const id = req.query.id;
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = parseInt(req.query.limit);
+  // console.log(`{id: ${id}, from: ${from}, to: ${to}, limit: ${limit}}`);
+
+  let query = User.find();
+
+  // Test: http://localhost:5000/api/exercise/log?from=1987-09-09
+  if (from) {
+    query.where("date").gte(from);
+  }
+  // Test: http://localhost:5000/api/exercise/log?to=2018-09-09
+  if (to) {
+    query.where("date").lte(to);
+  }
+
+  query
+    // Didn't run into the same issues with limit() as with lte() and gte(). If
+    // no value for limit is passed, then limit() doesn't seem to query while
+    // gte() and lte() do ... temporary solution was to create the above if
+    // conditionals. TODO: Figure out why the difference?
+    .limit(limit)
     .populate()
     .then(users => {
       if (users.length === 0) {
@@ -39,7 +62,7 @@ server.get("/api/users/", (req, res) => {
 server.post("/api/exercise/new-user/", (req, res) => {
   const data = req.body;
   User.create(data)
-    .then(data => res.status(201).json({ data }))
+    .then(data => res.status(201).json(data))
     .catch(error => res.status(500).json({ error }));
 });
 
@@ -49,18 +72,32 @@ server.put("/api/exercise/add/", (req, res, done) => {
   User.findOneAndUpdate(
     { _id: data._id },
     {
-        description: data.description,
-        duration: data.duration,
-        date: data.date
+      description: data.description,
+      duration: data.duration,
+      date: data.date
     },
     { new: true },
+    { runValidators: true },
     (err, data) => {
       if (err) {
-        res.status(500).json({ error })
+        res.status(500).json({ error });
       }
-      res.status(201).json(data)
+      res.status(201).json(data);
     }
-  )
+  );
+});
+
+// Log One User by Id
+server.get("/api/exercise/log/:id", (req, res) => {
+  const personId = req.params.id;
+  User.findById(personId)
+    .populate()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 // Listen to see if everything is working
