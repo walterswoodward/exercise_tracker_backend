3e51f9e1c006d5171bb01279d16898743cbbdb83
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-11-22 23:38:55 -0500

    userId type correct String => Number

--- models/Exercise.js
+++ models/Exercise.js
@@ -8,7 +8,7 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema(
   {
     username: { type: String, required: true},
-    userId: {type: Number, required: true },
+    userId: {type: String, required: true },
     description: {type: String, required: true},
     duration: {type: Number, required: true },
     date: {type: Date, required: true }
